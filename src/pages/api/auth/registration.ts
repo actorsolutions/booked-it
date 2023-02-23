443b0788f7ba0f00caabda79d4f414da0d716d6a
Zach De Nardi <zdenardi@gmail.com>
2023-02-23 00:30:17 -0800

    BI-registration- finished up registration for AUTH0,

--- src/pages/api/auth/registration.ts
+++ src/pages/api/auth/registration.ts
@@ -1,16 +1,32 @@
 import { PrismaClient } from '@prisma/client'
+import {NextApiRequest, NextApiResponse} from "next";
 /***
  * Hook for Auth0 to send user information for prisma
  * */
 
-const Registration = async(req,res)=>{
-    const {email} = JSON.parse(req.body);
-    console.log('Creating User!')
-    console.log(email)
+const Registration = async(req:NextApiRequest,res:NextApiResponse)=>{
+    console.log("hitting reg")
+    const {email,secret} = JSON.parse(req.body);
+    const prisma = new PrismaClient();
+    console.log(secret)
+    if(secret === process.env.AUTH0_HOOK_SECRET){
+        try {
+            const newUser = await prisma.user.create({
+                data:{email},
+            })
+            console.log('Creating User!')
+        }catch(err){
+            console.log(err)
+        }finally{
+            await prisma.$disconnect();
+        }
+        res.send({received:true});
+    }else{
+        console.log("No secret!");
+        res.status(500)
+    }
 
-    //TODO - CREATE USER
 
-    res.send({received:true});
 }
 
 export default Registration
