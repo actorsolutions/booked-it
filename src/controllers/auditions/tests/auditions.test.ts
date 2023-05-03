8ec5dd2eb519224541d379161fbfea3763a3e3cb
tyleresselman <108547751+tyleresselman@users.noreply.github.com>
2023-05-03 15:56:54 -0700

    Update src/controllers/auditions/tests/auditions.test.ts
    Co-authored-by: Zach De Nardi <32071166+zdenardi@users.noreply.github.com>

--- src/controllers/auditions/tests/auditions.test.ts
+++ src/controllers/auditions/tests/auditions.test.ts
@@ -251,7 +251,6 @@ describe("Audition Controller Tests", () => {
       fakeResp as never as NextApiResponse,
       mockDb as never
     );
-    // @ts-ignore
     expect(finalBody).toEqual(audition);
     expect(finalStatusCode).toEqual(200);
   });
