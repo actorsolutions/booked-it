7b14af7b2ab494b18003f92b8adf81f44db5b69b
tyleresselman <tyler.esselman@landiindustries.com>
2023-05-06 18:46:05 -0700

    BI-27 - began building user controller

---
+++ src/controllers/users/index.ts
@@ -0,0 +1,34 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { Users } from "../../models/Users";
+import { getSession } from "@auth0/nextjs-auth0";
+import { prisma } from "../../utils/prisma";
+
+export const getUserByEmail = async (
+    req: NextApiRequest,
+    res: NextApiResponse,
+    db = prisma.user
+) => {
+    const session = await getSession(req, res);
+    const email = session?.user.email;
+    const user = await Users.findByEmail(email, db);
+    if (email) {
+        res.status(200).send({ user })
+    } else {
+        res.status(500).send({error: 'No User with that email.'})
+    }
+};
+
+export const getUserById = async (
+    req: NextApiRequest,
+    res: NextApiResponse,
+    db = prisma.user
+) => {
+    const session = await getSession(req, res);
+    const userId = parseInt(session?.user.id);
+    const { id } = req.query;
+    const user = await Users.findById(parseInt(id as string), db);
+    if (user?.id != userId) {
+        return res.status(401).send({ message: "Unauthorized" });
+    }
+    return res.status(200).send(user)
+}
