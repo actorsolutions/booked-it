cca5d368d4322b04c4680e29cb3a07adad366936
Zach De Nardi <zdenardi@gmail.com>
2023-05-12 14:59:36 -0700

    BI-10-LoginComponent-Added Material UI to main layout

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,38 +1,42 @@
 import Container from "@mui/material/Container";
-import Box from '@mui/material/Box'
+import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import {Button, Grid} from "@mui/material";
+import { Button, Grid, Card } from "@mui/material";
 
 export const Login = () => {
   return (
-    <Container component="main" maxWidth='xs'  >
+    <Container component="main" maxWidth="xs">
+      <Card>
         <Grid
-            container
-            spacing={0}
-            direction="column"
-            alignItems="center"
-            justifyContent="center"
-            style={{ minHeight: '100vh' }}>
-            <Box sx={{
-                marginTop:8,
-                display:"flex",
-                flexDirection:"column",
-                alignItems:"center",
-
-            }}>
-                <Typography component="h1" variant="h5">
-                    Welcome to Booked-It!
-                </Typography>
-                <Button
-                    type="submit"
-                    fullWidth
-                    variant="contained"
-                    href={"api/auth/login"}
-                >Login or Sign Up</Button>
-
-            </Box>
+          container
+          spacing={0}
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+          style={{ minHeight: "100vh" }}
+        >
+          <Box
+            sx={{
+              marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Typography component="h1" variant="h5">
+              Welcome to Booked-It!
+            </Typography>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              href={"api/auth/login"}
+            >
+              Login or Sign Up
+            </Button>
+          </Box>
         </Grid>
-
+      </Card>
     </Container>
   );
 };
