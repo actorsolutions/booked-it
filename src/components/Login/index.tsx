7e75720186b21960fc58961e36007dc783d3a391
Zach De Nardi <zdenardi@gmail.com>
2023-05-12 12:07:41 -0700

    BI-10-LoginComponent-Simple Login Component

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,10 +1,38 @@
-import { Container } from "@mui/system";
-import { LoginButton } from "@/components/Auth/LoginButton";
+import Container from "@mui/material/Container";
+import Box from '@mui/material/Box'
+import Typography from "@mui/material/Typography";
+import {Button, Grid} from "@mui/material";
 
 export const Login = () => {
   return (
-    <Container maxWidth="md">
-      <a href={"api/auth/login"}>Login</a>
+    <Container component="main" maxWidth='xs'  >
+        <Grid
+            container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justifyContent="center"
+            style={{ minHeight: '100vh' }}>
+            <Box sx={{
+                marginTop:8,
+                display:"flex",
+                flexDirection:"column",
+                alignItems:"center",
+
+            }}>
+                <Typography component="h1" variant="h5">
+                    Welcome to Booked-It!
+                </Typography>
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    href={"api/auth/login"}
+                >Login or Sign Up</Button>
+
+            </Box>
+        </Grid>
+
     </Container>
   );
 };
