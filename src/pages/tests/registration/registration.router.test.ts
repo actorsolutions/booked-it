c6bd736c26d5164d493edc86ac22710869f8f295
tyleresselman <tyler.esselman@landiindustries.com>
2023-05-15 10:48:57 -0700

    BI-46 - adjusted config to exclude cypress on root level...for now

--- src/pages/tests/registration/registration.router.test.ts
+++ src/pages/tests/registration/registration.router.test.ts
@@ -6,7 +6,6 @@ import {
     SESSION_DATA,
     tearDown
 } from "@/utils/testSetup";
-import { expect } from '@jest/globals';
 import { generateSessionCookie } from "@auth0/nextjs-auth0/testing";
 
 describe('Registration router integration tests', () => {
