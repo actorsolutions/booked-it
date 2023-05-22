0827d687f4fd510dc6b5229351baeeff42ac724c
tyleresselman <tme91302@gmail.com>
2023-05-22 15:19:48 -0700

    BI-18 - added accordion element, working on structure to fit audition info properly

--- src/components/AuditionRow.tsx
+++ src/components/AuditionRow.tsx
@@ -1,5 +1,10 @@
+import { useState } from "react";
 import { Card, Grid } from "@mui/material";
 import LensIcon from "@mui/icons-material/Lens";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
 import { Audition, Casting } from "@/types";
 import CY_TAGS from "@/support/cypress_tags";
 
@@ -28,6 +33,12 @@ export const AuditionRow = ({ audition, index }: AuditionRowProps) => {
     }
     const casting = audition.casting ? (audition.casting as Array<Casting>) : [];
 
+    const [expanded, setExpanded] = useState(false);
+
+    const handleAccordionChange = () => {
+        setExpanded(!expanded);
+    };
+
     return (
         <Card
             sx={{
@@ -40,15 +51,29 @@ export const AuditionRow = ({ audition, index }: AuditionRowProps) => {
             key={audition.id}
             data-cy={`${AUDITIONS_SECTION.CONTAINERS.AUDITION_ROW}${index}`}
         >
+            <Accordion expanded={expanded} onChange={handleAccordionChange}>
             <Grid container spacing={2}>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    id="accordion-header"
+                    sx={{ width: "100%" }}
+                >
                 <Grid item xs={2}>
                     <LensIcon color={statusColor(audition.status)} fontSize="large" />
                 </Grid>
                 <Grid item xs={7}>
                     <div> Project: {audition.project} </div>
                 </Grid>
+                </AccordionSummary>
+                <AccordionDetails sx={{ width: "100%" }} >
+                <Grid item xs={2}>
+                    <div> Type: {audition.type} </div>
+                </Grid>
                 <Grid item md={3}>
                     <div>{casting.length > 0 ? casting[0].name : undefined}</div>
+                </Grid>
+                <Grid item md={3}>
+                    <div>Notes: {audition.notes}</div>
                 </Grid>
                 <Grid item xs={2}>
                     <div style={{ justifyContent: "flex-end" }}>
@@ -57,7 +82,24 @@ export const AuditionRow = ({ audition, index }: AuditionRowProps) => {
                         })}
                     </div>
                 </Grid>
+                </AccordionDetails>
             </Grid>
+            </Accordion>
         </Card>
+
     );
 };
+
+// <Grid>
+//     <Accordion>
+//         <AccordionSummary
+//             expandIcon={<ExpandMoreIcon />}
+//             id="accordion-header"
+//         >
+//             Test Accordion
+//         </AccordionSummary>
+//         <AccordionDetails>
+//             Test test test
+//         </AccordionDetails>
+//     </Accordion>
+// </Grid>
