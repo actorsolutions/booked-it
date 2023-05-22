67adeb2e61ac538d22e28354c41e5cc2f4e379fb
tyleresselman <tme91302@gmail.com>
2023-05-22 13:29:20 -0700

    BI-18 - reworked with changes from master

--- src/components/AuditionRow.tsx
+++ src/components/AuditionRow.tsx
@@ -4,11 +4,11 @@ import { Audition, Casting } from "@/types";
 import CY_TAGS from "@/support/cypress_tags";
 
 interface AuditionRowProps {
-  audition: Audition;
-  index: number;
+    audition: Audition;
+    index: number;
 }
 
-export const AuditionRow = ({ audition }: AuditionRowProps) => {
+export const AuditionRow = ({ audition, index }: AuditionRowProps) => {
     const { AUDITIONS_SECTION } = CY_TAGS;
     const statusColor = (status: string): "info" | "secondary" | "warning" | "error" | "success" | "disabled" => {
         switch (status) {
@@ -26,38 +26,38 @@ export const AuditionRow = ({ audition }: AuditionRowProps) => {
                 return "disabled"
         }
     }
-  const casting = audition.casting ? (audition.casting as Array<Casting>) : [];
+    const casting = audition.casting ? (audition.casting as Array<Casting>) : [];
 
-  return (
-    <Card
-      sx={{
-        alignItems: "center",
-        pl: "4px",
-        backgroundColor: "white",
-        display: "flex",
-        minHeight: "4.5rem",
-      }}
-      key={audition.id}
-      data-cy={`${AUDITIONS_SECTION.CONTAINERS.AUDITION_ROW}${index}`}
-    >
-      <Grid container spacing={2}>
-        <Grid item xs={2}>
-          <LensIcon color={statusColor(audition.status)} fontSize="large" />
-        </Grid>
-        <Grid item xs={7}>
-          <div> Project: {audition.project} </div>
-        </Grid>
-        <Grid item md={3}>
-          <div>{casting.length > 0 ? casting[0].name : undefined}</div>
-        </Grid>
-        <Grid item xs={2}>
-          <div style={{ justifyContent: "flex-end" }}>
-            {new Date(audition.date * 1000).toLocaleDateString("en", {
-              dateStyle: "short",
-            })}
-          </div>
-        </Grid>
-      </Grid>
-    </Card>
-  );
+    return (
+        <Card
+            sx={{
+                alignItems: "center",
+                pl: "4px",
+                backgroundColor: "white",
+                display: "flex",
+                minHeight: "4.5rem",
+            }}
+            key={audition.id}
+            data-cy={`${AUDITIONS_SECTION.CONTAINERS.AUDITION_ROW}${index}`}
+        >
+            <Grid container spacing={2}>
+                <Grid item xs={2}>
+                    <LensIcon color={statusColor(audition.status)} fontSize="large" />
+                </Grid>
+                <Grid item xs={7}>
+                    <div> Project: {audition.project} </div>
+                </Grid>
+                <Grid item md={3}>
+                    <div>{casting.length > 0 ? casting[0].name : undefined}</div>
+                </Grid>
+                <Grid item xs={2}>
+                    <div style={{ justifyContent: "flex-end" }}>
+                        {new Date(audition.date * 1000).toLocaleDateString("en", {
+                            dateStyle: "short",
+                        })}
+                    </div>
+                </Grid>
+            </Grid>
+        </Card>
+    );
 };
