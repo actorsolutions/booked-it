675f65a65ea2d5e7997e04a0ff5750c9c76f5f52
tyleresselman <tme91302@gmail.com>
2023-06-07 14:06:24 -0500

    BI-47 - merging

---
+++ src/components/common/Dialogs/AddEditAuditionDialog.tsx
@@ -0,0 +1,37 @@
+import Dialog from "@mui/material/Dialog";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import { AuditionForm } from "@/components/AuditionForm";
+import React, { Dispatch, SetStateAction } from "react";
+import { Audition } from "@/types";
+import CY_TAGS from "@/support/cypress_tags";
+interface Props {
+  audition?: Audition;
+  auditions: Audition[];
+  setAuditions: Dispatch<SetStateAction<Audition[]>>;
+  handleClose: () => void;
+  open: boolean;
+}
+export const AddEditAuditionDialog = (props: Props) => {
+  const { audition, auditions, setAuditions, handleClose, open } = props;
+  return (
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="audition-form"
+      aria-describedby="audition-form-modal"
+    >
+      <DialogContent>
+        <DialogTitle data-cy={CY_TAGS.AUDITION_FORM.TITLE}>
+          {audition ? "Edit Audition" : "Add Audition"}
+        </DialogTitle>
+        <AuditionForm
+          auditions={auditions}
+          setAuditions={setAuditions}
+          handleClose={handleClose}
+          audition={audition}
+        />
+      </DialogContent>
+    </Dialog>
+  );
+};
