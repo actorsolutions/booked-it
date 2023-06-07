6debcf59b7ad543ed92ba199afd2c64761cccb20
tyleresselman <tme91302@gmail.com>
2023-06-07 14:36:15 -0500

    BI-47 - merging

--- src/components/AuditionForm/AuditionForm.tsx
+++ src/components/AuditionForm/AuditionForm.tsx
@@ -31,15 +31,10 @@ interface Props {
   handleClose: () => void;
 }
 export const AuditionForm = (props: Props) => {
-  const { setAuditions, auditions, handleClose, audition } = props;
-  const { AUDITION_FORM } = CY_TAGS;
-  const { AUDITIONS } = SNACKBAR_MESSAGES
-  const { setAuditions, auditions, handleClose } = props;
+  const {setAuditions, auditions, handleClose, audition} = props;
+  const {AUDITION_FORM} = CY_TAGS;
+  const {AUDITIONS} = SNACKBAR_MESSAGES
   const {showSnackBar} = useSnackBar();
-
-  const customValidation = async (arrayOfFields: fields[]) => {
-    return trigger(arrayOfFields as fields[], { shouldFocus: true });
-  };
 
   const [open, setOpen] = useState(false);
   const [submissionState, setSubmissionState] = useState<SubmissionState>({
@@ -48,7 +43,7 @@ export const AuditionForm = (props: Props) => {
   });
 
   const customValidation = async (arrayOfFields: fields[]) => {
-    return trigger(arrayOfFields as fields[], { shouldFocus: true });
+    return trigger(arrayOfFields as fields[], {shouldFocus: true});
   };
 
   const {
@@ -57,7 +52,7 @@ export const AuditionForm = (props: Props) => {
     watch,
     setValue,
     register,
-    formState: { errors },
+    formState: {errors},
     trigger,
     clearErrors,
     reset,
@@ -77,20 +72,22 @@ export const AuditionForm = (props: Props) => {
 
   const MAX_CASTING_ROWS = 2;
   const [castingRowCount, setCastingRowCount] = useState(
-    getValues().casting?.length || 0
+      getValues().casting?.length || 0
   );
+
   interface SubmissionState {
     loading: boolean;
     submitted: boolean;
   }
+
   type fields =
-    | "date"
-    | "project"
-    | "company"
-    | "callbackDate"
-    | "notes"
-    | "type"
-    | "status";
+      | "date"
+      | "project"
+      | "company"
+      | "callbackDate"
+      | "notes"
+      | "type"
+      | "status";
 
   const createFields = [
     "date",
@@ -135,171 +132,173 @@ export const AuditionForm = (props: Props) => {
         };
         const response = await updateAudition(updateData as Audition);
         const auditionToReplace = auditions.find(
-          (audition) => audition.id === response.id
+            (audition) => audition.id === response.id
         ) as Audition;
         Object.assign(auditionToReplace, response);
         setCastingRowCount(watchCasting ? watchCasting.length : 0);
         return true;
       } else {
         try {
-        const addedAudition = await createAudition(getValues());
-        auditions.push(addedAudition);
-        setAuditions(auditions);
-        setSubmissionState({
-          loading: false,
-          submitted: true,
-        });
-        showSnackBar(AUDITIONS.AUDITION_CREATE_SUCCESS, "success");
-        setCastingRowCount(watchCasting ? watchCasting.length : 0);
-        return true;
-      } catch (error) {
-        console.log(error);
-        showSnackBar(AUDITIONS.AUDITION_CREATE_FAILURE, "error");
+          const addedAudition = await createAudition(getValues());
+          auditions.push(addedAudition);
+          setAuditions(auditions);
+          setSubmissionState({
+            loading: false,
+            submitted: true,
+          });
+          showSnackBar(AUDITIONS.AUDITION_CREATE_SUCCESS, "success");
+          setCastingRowCount(watchCasting ? watchCasting.length : 0);
+          return true;
+        } catch (error) {
+          console.log(error);
+          showSnackBar(AUDITIONS.AUDITION_CREATE_FAILURE, "error");
+        }
       }
-    }
       setSubmissionState({
         loading: false,
         submitted: false,
       });
       showSnackBar(AUDITIONS.AUDITION_CREATE_VALIDATION_ERROR, "error")
       return false;
-  };
-
-  // TODO - BI-72 Refactor handleDeleteCastingRow to live in CastingList
-  const handleDeleteCastingRow = (index: number) => {
-    const updatedCasting = [...(watchCasting || [])];
-    updatedCasting.splice(index, 1);
-    setValue("casting", updatedCasting);
-  };
-
-  const setCasting = (castingArray: Casting[]) => {
-    setValue("casting", castingArray);
-  };
-  useEffect(() => {
-    if (audition) {
-      const data = {
-        archived: audition.archived,
-        callbackDate: audition.callBackDate
-          ? audition.callBackDate * 1000
-          : undefined,
-        casting: audition.casting || [],
-        company: audition.company,
-        date: audition.date * 1000,
-        notes: audition.notes,
-        project: audition.project,
-        status: audition.status,
-        type: audition.type,
-      };
-      reset({ ...data });
     }
-  }, [audition, reset]);
 
-  return (
-    <Container
-      data-cy={AUDITION_FORM.CONTAINERS.FORM_CONTAINER}
-      maxWidth="md"
-      id="auditionModal"
-    >
-      <Divider />
-      <Form>
-        <Grid item sm={8} md={6}>
-          <AuditionDatePicker control={control} register={register} />
-          {errors.date && (
-            <Typography data-cy={AUDITION_FORM.ERRORS.DATE} variant="overline">
-              Required!
-            </Typography>
-          )}
-        </Grid>
-        <Grid item sm={8} md={6}>
-          <ProjectInput control={control} register={register} />
-          {errors.project && (
-            <Typography
-              data-cy={AUDITION_FORM.ERRORS.PROJECT}
-              variant="overline"
-            >
-              Required!
-            </Typography>
-          )}
-          <CompanyInput control={control} register={register} />
-          {errors.company && (
-            <Typography
-              data-cy={AUDITION_FORM.ERRORS.COMPANY}
-              variant="overline"
-            >
-              Required!
-            </Typography>
-          )}
-        </Grid>
-        <Grid item sm={8} md={6}>
-          <StatusDropdown control={control} register={register} />
-          {errors.status && (
-            <Typography
-              data-cy={AUDITION_FORM.ERRORS.STATUS}
-              variant="overline"
-            >
-              Required!
-            </Typography>
-          )}
-          <TypeDropdown control={control} register={register} />
-          {errors.type && (
-            <Typography data-cy={AUDITION_FORM.ERRORS.TYPE} variant="overline">
-              Required!
-            </Typography>
-          )}
-        </Grid>
-        {watchStatus === "callback" && (
-          <CallbackPicker control={control} register={register} />
-        )}
-        <Grid item sm={8} md={6}>
-          <NotesTextArea control={control} register={register} />
-        </Grid>
-        <Grid item sm={8}>
-          {watchCasting ? (
-            <CastingList
-              casting={watchCasting}
-              onDelete={handleDeleteCastingRow}
-              name={""}
-              listCyTag={AUDITION_FORM.CASTING.CASTING_LIST}
-            />
-          ) : null}
-        </Grid>
-        {watchCasting && watchCasting.length < MAX_CASTING_ROWS && (
-          <Grid item sm={8} md={6}>
-            <Button
-              data-cy={AUDITION_FORM.BUTTONS.ADD_CASTING}
-              onClick={handleModal}
-            >
-              Add Casting
-            </Button>
-            <Dialog open={open} onClose={handleModal}>
-              <DialogContent>
-                <CastingForm
-                  auditionControl={control}
-                  initialCastingList={getValues().casting}
-                  setCasting={setCasting}
-                  handleClose={handleModal}
-                />
-              </DialogContent>
-            </Dialog>
-          </Grid>
-        )}
-        <Button
-          data-cy={
-            audition
-              ? AUDITION_FORM.BUTTONS.EDIT_AUDITION
-              : AUDITION_FORM.BUTTONS.ADD_AUDITION
-          }
-          onClick={() => {
-            clearErrors();
-            handleCreateOrEdit().then((wasSent) => {
-              wasSent && handleClose();
-            });
-          }}
+
+    // TODO - BI-72 Refactor handleDeleteCastingRow to live in CastingList
+    const handleDeleteCastingRow = (index: number) => {
+      const updatedCasting = [...(watchCasting || [])];
+      updatedCasting.splice(index, 1);
+      setValue("casting", updatedCasting);
+    };
+
+    const setCasting = (castingArray: Casting[]) => {
+      setValue("casting", castingArray);
+    };
+    useEffect(() => {
+      if (audition) {
+        const data = {
+          archived: audition.archived,
+          callbackDate: audition.callBackDate
+              ? audition.callBackDate * 1000
+              : undefined,
+          casting: audition.casting || [],
+          company: audition.company,
+          date: audition.date * 1000,
+          notes: audition.notes,
+          project: audition.project,
+          status: audition.status,
+          type: audition.type,
+        };
+        reset({...data});
+      }
+    }, [audition, reset]);
+
+    return (
+        <Container
+            data-cy={AUDITION_FORM.CONTAINERS.FORM_CONTAINER}
+            maxWidth="md"
+            id="auditionModal"
         >
-          {audition ? "Edit Audition" : "Add Audition"}
-        </Button>
-        {submissionState.loading && <LoadingCircle />}
-      </Form>
-    </Container>
-  );
-};
+          <Divider/>
+          <Form>
+            <Grid item sm={8} md={6}>
+              <AuditionDatePicker control={control} register={register}/>
+              {errors.date && (
+                  <Typography data-cy={AUDITION_FORM.ERRORS.DATE} variant="overline">
+                    Required!
+                  </Typography>
+              )}
+            </Grid>
+            <Grid item sm={8} md={6}>
+              <ProjectInput control={control} register={register}/>
+              {errors.project && (
+                  <Typography
+                      data-cy={AUDITION_FORM.ERRORS.PROJECT}
+                      variant="overline"
+                  >
+                    Required!
+                  </Typography>
+              )}
+              <CompanyInput control={control} register={register}/>
+              {errors.company && (
+                  <Typography
+                      data-cy={AUDITION_FORM.ERRORS.COMPANY}
+                      variant="overline"
+                  >
+                    Required!
+                  </Typography>
+              )}
+            </Grid>
+            <Grid item sm={8} md={6}>
+              <StatusDropdown control={control} register={register}/>
+              {errors.status && (
+                  <Typography
+                      data-cy={AUDITION_FORM.ERRORS.STATUS}
+                      variant="overline"
+                  >
+                    Required!
+                  </Typography>
+              )}
+              <TypeDropdown control={control} register={register}/>
+              {errors.type && (
+                  <Typography data-cy={AUDITION_FORM.ERRORS.TYPE} variant="overline">
+                    Required!
+                  </Typography>
+              )}
+            </Grid>
+            {watchStatus === "callback" && (
+                <CallbackPicker control={control} register={register}/>
+            )}
+            <Grid item sm={8} md={6}>
+              <NotesTextArea control={control} register={register}/>
+            </Grid>
+            <Grid item sm={8}>
+              {watchCasting ? (
+                  <CastingList
+                      casting={watchCasting}
+                      onDelete={handleDeleteCastingRow}
+                      name={""}
+                      listCyTag={AUDITION_FORM.CASTING.CASTING_LIST}
+                  />
+              ) : null}
+            </Grid>
+            {watchCasting && watchCasting.length < MAX_CASTING_ROWS && (
+                <Grid item sm={8} md={6}>
+                  <Button
+                      data-cy={AUDITION_FORM.BUTTONS.ADD_CASTING}
+                      onClick={handleModal}
+                  >
+                    Add Casting
+                  </Button>
+                  <Dialog open={open} onClose={handleModal}>
+                    <DialogContent>
+                      <CastingForm
+                          auditionControl={control}
+                          initialCastingList={getValues().casting}
+                          setCasting={setCasting}
+                          handleClose={handleModal}
+                      />
+                    </DialogContent>
+                  </Dialog>
+                </Grid>
+            )}
+            <Button
+                data-cy={
+                  audition
+                      ? AUDITION_FORM.BUTTONS.EDIT_AUDITION
+                      : AUDITION_FORM.BUTTONS.ADD_AUDITION
+                }
+                onClick={() => {
+                  clearErrors();
+                  handleCreateOrEdit().then((wasSent) => {
+                    wasSent && handleClose();
+                  });
+                }}
+            >
+              {audition ? "Edit Audition" : "Add Audition"}
+            </Button>
+            {submissionState.loading && <LoadingCircle/>}
+          </Form>
+        </Container>
+    );
+  };
+}
