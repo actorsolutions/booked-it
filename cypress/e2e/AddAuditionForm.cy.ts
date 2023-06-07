fe83e389d95db6253d2222752bf291be1b3ff422
tyleresselman <tme91302@gmail.com>
2023-06-06 19:32:34 -0500

    BI-47 - building cypress coverage

--- cypress/e2e/AddAuditionForm.cy.ts
+++ cypress/e2e/AddAuditionForm.cy.ts
@@ -125,4 +125,37 @@ describe("Add Auditions Form E2E Tests", () => {
       "be.visible"
     );
   });
+  it.only("Should show success Snackbar message when audition is created", () => {
+    cy.task("db:seed");
+    login();
+    cy.visit("/");
+    cy.wait("@Auth0");
+
+    findAndClick(AUDITIONS_SECTION.BUTTONS.CREATE_AUDITION);
+
+    cy.get(cyTag(AUDITION_FORM.CONTAINERS.FORM_CONTAINER)).should("be.visible");
+
+    //TODO: (BI-59) Better Datepicker Logic
+    //eslint-disable-next-line cypress/unsafe-to-chain-command
+    cy.get(cyTag(AUDITION_FORM.PICKERS.DATE)).click().type("01012023");
+
+    addSelectItem(
+        AUDITION_FORM.DROPDOWNS.TYPE,
+        AUDITION_FORM.DROPDOWNS.OPTIONS.TYPE,
+        "Television"
+    );
+    addSelectItem(
+        AUDITION_FORM.DROPDOWNS.STATUS,
+        AUDITION_FORM.DROPDOWNS.OPTIONS.STATUS,
+        "Booked"
+    );
+
+    addToInput(AUDITION_FORM.INPUTS.PROJECT, "WallyWorld");
+    addToInput(AUDITION_FORM.INPUTS.COMPANY, "WallyCorp");
+    addToInput(AUDITION_FORM.TEXT_AREA.NOTES, "Wally is a good boy");
+    findAndClick(AUDITION_FORM.BUTTONS.ADD_AUDITION);
+    shouldNotExist(AUDITION_FORM.CONTAINERS.FORM_CONTAINER);
+
+    cy.contains("AUDITIONS.AUDITION_CREATE_SUCCESS").should("be.visible");
+  });
 });
