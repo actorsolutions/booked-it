55f756bde8ed85747e2eac7c769664d49d553549
zachdenardi <zdenardi@gmail.com>
2023-06-14 14:25:16 -0700

    BI-54-AddedMaterialUiTheme-dashboardWrapper added!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,29 +5,33 @@ import { SnackBarProvider } from "@/context/SnackbarContext";
 import { Container } from "@mui/material";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { ThemeProvider } from "@mui/material/styles";
+import { theme } from "@/support/MaterialUITheme";
 import React from "react";
 
 export default function Home() {
   return (
-    <UserProvider>
-      <SnackBarProvider>
-        <main>
-          <Container
-            id={"MainContainer"}
-            maxWidth={false}
-            sx={{
-              minHeight: "100vh",
-              bgcolor: "#caccce",
-              mt: "1rem",
-              pb: "2rem",
-            }}
-          >
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <Dashboard />
-            </LocalizationProvider>
-          </Container>
-        </main>
-      </SnackBarProvider>
-    </UserProvider>
+    <ThemeProvider theme={theme}>
+      <UserProvider>
+        <SnackBarProvider>
+          <main>
+            <Container
+              id={"MainContainer"}
+              maxWidth={false}
+              sx={{
+                minHeight: "100vh",
+                bgcolor: "#caccce",
+                mt: "1rem",
+                pb: "2rem",
+              }}
+            >
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <Dashboard />
+              </LocalizationProvider>
+            </Container>
+          </main>
+        </SnackBarProvider>
+      </UserProvider>
+    </ThemeProvider>
   );
 }
---
+++ src/support/MaterialUITheme.ts
@@ -0,0 +1,27 @@
+import { createTheme } from "@mui/material/styles";
+
+declare module "@mui/material/styles" {}
+declare module "@mui/material/Paper" {
+  // eslint-disable-next-line no-unused-vars
+  interface PaperPropsVariantOverrides {
+    dashboardWrapper: true;
+  }
+}
+
+export const theme = createTheme({
+  components: {
+    MuiPaper: {
+      variants: [
+        {
+          props: { variant: "dashboardWrapper" },
+          style: {
+            padding: "1rem",
+            backgroundColor: "snow",
+            boxShadow:
+              "0px 3px 5px -1px rgba(0,0,0,0.2),0px 5px 8px 0px rgba(0,0,0,0.14),0px 1px 14px 0px rgba(0,0,0,0.12)",
+          },
+        },
+      ],
+    },
+  },
+});
