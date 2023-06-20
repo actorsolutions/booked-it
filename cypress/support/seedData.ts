b58df2a4fa9d84ce31786fa1493a0860e36b426c
zachdenardi <zdenardi@gmail.com>
2023-06-20 15:24:21 -0700

    BI-95-StatusChangesStatus_modelTest-typing status/types

--- cypress/support/seedData.ts
+++ cypress/support/seedData.ts
@@ -1,3 +1,4 @@
+import { audition_types, audition_status } from "prisma";
 export const USER_DATA = {
   id: 0,
   sid: "1MRVj_nf2Gy4RSjaNjTIaUZWX3Djf0w-",
@@ -9,11 +10,11 @@ export const AUDITION_DATA = {
   id: 0,
   notes: "Here is a note",
   project: "Test Project",
-  type: "television",
+  type: "television" as audition_types,
   userId: 0,
   company: "Test Company",
   createdAt: "2023-04-28T21:50:11.638Z",
-  status: "scheduled",
+  status: "scheduled" as audition_status,
   archived: false,
   casting: [],
 };
