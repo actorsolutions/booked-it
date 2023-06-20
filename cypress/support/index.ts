904694eeb1cc2e74f4ea9e99318e634b13d43392
zachdenardi <zdenardi@gmail.com>
2023-06-20 15:35:49 -0700

    BI-95-StatusChangesStatus_modelTest-got rid of shortened path

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import { generatePrisma } from "@/utils/prisma";
+import { generatePrisma } from "../../src/utils/prisma";
 
 /**
  * Replicates sanitizeDB functionality in the cypress portion of the product
