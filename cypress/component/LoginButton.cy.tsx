b6de8e179ad9bcedb639ca44ac4d138edd4c9147
tyleresselman <tme91302@gmail.com>
2023-07-05 14:15:16 -0700

    BI-110 - refactored the login button test, just for consistency

--- cypress/component/LoginButton.cy.tsx
+++ cypress/component/LoginButton.cy.tsx
@@ -1,10 +1,17 @@
 import React from "react";
-import "@/app/setup";
+import { setupApp } from "@/app/setup";
+import { DashboardWrapper } from "@/components/common/Layout/DashboardWrapper";
 import { LoginButton } from "@/components/Auth/LoginButton";
 
 describe("<LoginButton />", () => {
   it("renders", () => {
     // see: https://on.cypress.io/mounting-react
-    cy.mount(<LoginButton />);
+    cy.mount(
+      setupApp(
+        <DashboardWrapper>
+          <LoginButton />
+        </DashboardWrapper>
+      )
+    );
   });
 });
