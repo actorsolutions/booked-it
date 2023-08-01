3f04a46538f60c4dd5bc6bb194d4197a2a01a0d2
zachdenardi <zdenardi@gmail.com>
2023-08-01 15:35:44 -0700

    BI-123-AG-Grid-Linter is being a pain in Github, added more type

--- src/components/Tables/ActorsAccess/index.tsx
+++ src/components/Tables/ActorsAccess/index.tsx
@@ -3,7 +3,7 @@ import { AgGridReact } from "ag-grid-react";
 import React, { useEffect, useMemo, useRef, useState } from "react";
 import { scrapeAuditions } from "@/apihelpers/actorsAccess";
 import { SelectTypeRenderer } from "@/components/Tables/ActorsAccess/CustomSelectCell";
-import { IRowNode, ValueFormatterParams } from "ag-grid-community";
+import { ColDef, IRowNode, ValueFormatterParams } from "ag-grid-community";
 import { CreateAuditionData } from "@/types";
 
 interface ActorsAccessData {
@@ -65,7 +65,7 @@ export const ActorsAccessGrid = () => {
     const date = params.data.date;
     return new Date(date).toLocaleDateString("en-US");
   }
-  const columnDefs: any = [
+  const columnDefs: ColDef[] = [
     { field: "project" },
     { field: "role" },
     { field: "casting" },
