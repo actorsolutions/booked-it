11152f116390fbe9d5ea0b1efcf0e590d700e6d8
tyleresselman <tme91302@gmail.com>
2023-08-10 22:55:38 -0700

    BI-133 - laid down logic for myAccount fetch

--- src/controllers/scrapers/castingnetworks.ts
+++ src/controllers/scrapers/castingnetworks.ts
@@ -18,27 +18,86 @@ export const getCastingNetworksSubmissions = async (
 }`,
   };
 
-  try {
-    const authenticateAndGetAccessToken = async () => {
-      const response = await axios({
-        url: castingNetworksGraphQL,
-        method: "post",
-        headers: authHeaders,
-        data: authQuery,
-      });
-
-      return response.data.data.verifyAccount.token.access;
-    };
-
-    const accessToken = await authenticateAndGetAccessToken();
-
-    console.log(accessToken);
-
-    res.status(200).send({
-      message: "Token received",
-      data: accessToken,
+  const authenticateAndGetAccessToken = async () => {
+    const response = await axios({
+      url: castingNetworksGraphQL,
+      method: "post",
+      headers: authHeaders,
+      data: authQuery,
     });
-  } catch (error) {
-    console.error("Error:", error);
-  }
+
+    return response.data.data.verifyAccount.token.access;
+  };
+
+  const accessToken = await authenticateAndGetAccessToken();
+
+  console.log(accessToken);
+
+  const fetchHeaders = {
+    "content-type": "application/json",
+    Authorization: `"${accessToken}"`,
+    Host: "",
+  };
+
+  const fetchQuery = {
+    query: `query MyAccount {
+    myAccount {
+        accountOrganizationDivisionsAccessCount
+        accountStatusId
+        countryId
+        created
+        email
+        emailResetToken
+        emailToReset
+        firstName
+        former
+        id
+        isAccountIPC
+        isAdmin
+        isCCD
+        isInPCContext
+        isIPC
+        languageLocaleId
+        lastArtistId
+        lastLogin
+        lastName
+        legacyEmail
+        legacyInstance
+        legacyLogin
+        linkedAccountDivisionCount
+        linkedOrganization
+        linkedToAccountId
+        loginAttemptCount
+        loginAttemptFailedAt
+        organizationDivisionsCount
+        passwordHash
+        phone
+        rosterInvitationToken
+        systemRoles
+        systemRolesIds
+        termsOfUse
+        updated
+    }
+}`,
+  };
+
+  const getAccountInfo = async () => {
+    const response = await axios({
+      url: castingNetworksGraphQL,
+      method: "post",
+      headers: fetchHeaders,
+      data: fetchQuery,
+    });
+
+    return response.data.data.myAccount;
+  };
+
+  const myAccount = await getAccountInfo();
+
+  console.log(myAccount);
+
+  res.status(200).send({
+    message: "Account information acquired",
+    data: myAccount,
+  });
 };
