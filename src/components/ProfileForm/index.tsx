914d2680a28bb6ef7fd7b1171cc1702977784f67
zachdenardi <zdenardi@gmail.com>
2023-09-19 21:43:00 -0700

    BI-144-ProfileForm-Started making components

---
+++ src/components/ProfileForm/index.tsx
@@ -0,0 +1,19 @@
+import { Control, FieldValues, UseFormRegister } from "react-hook-form";
+import { Representation } from "@/types/users";
+
+export interface ProfileFormData extends FieldValues {
+  firstName: string;
+  lastName: string;
+  email: string;
+  photo?: string;
+  AA_UN?: string;
+  AA_PW?: string;
+  CN_UN?: string;
+  CN_PW?: string;
+  representation?: Representation[];
+}
+
+export interface FormProps<T extends FieldValues> {
+  control: Control<T>;
+  register: UseFormRegister<T>;
+}
